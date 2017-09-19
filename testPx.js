d53d82c291830681ca0682225363604058a134ee
An <2103887953@qq.com>
2017-09-19 18:04:10 +0800

    update ui-test

--- testPx.js
+++ testPx.js
@@ -8,12 +8,9 @@ resemble.outputSettings({
         blue: 0
     },
     errorType: 'movement',
-    // transparency: 0.3,
-    // largeImageThreshold: 1200,
-    // useCrossOrigin: false
 })
-var diff = resemble('./images/pvp.jpg').compareTo('./images/1.png').ignoreColors().onComplete(function(data){
-    console.log(data)
-	fs.writeFileSync('./output.png', data.getBuffer());
+var diff = resemble('./images/pvp.jpg').compareTo('./images/2.png').ignoreColors().onComplete(function(data){
+    console.log('对比结果：' + data)
+	fs.writeFileSync('./images/output.png', data.getBuffer());
 })
 diff.ignoreColors();
