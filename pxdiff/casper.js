95ca671369782337521b9b77d5e9ad27d3c1fd8e
Aaaaaaaty <2103887953@qq.com>
2017-09-27 11:52:18 +0800

    test

--- pxdiff/casper.js
+++ pxdiff/casper.js
@@ -7,7 +7,6 @@ const casper = require('casper').create({
         height: 4080
     }
 })
-console.log(1)
 const fileName = decodeURIComponent(system.args[4])
 const url = decodeURIComponent(system.args[5])
 const selector = decodeURIComponent(system.args[6])
